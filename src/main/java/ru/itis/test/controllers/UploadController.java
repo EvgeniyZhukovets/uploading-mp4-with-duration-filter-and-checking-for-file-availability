5235afb50f515e35b7f5c2a5e4a0d0b20cb64d63
Evgeniy Zhukovets <evzhukovets@yandex.ru>
2019-10-13 22:00:14 +0300

    complete version

--- src/main/java/ru/itis/test/controllers/UploadController.java
+++ src/main/java/ru/itis/test/controllers/UploadController.java
@@ -1,16 +1,16 @@
 package ru.itis.test.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.http.HttpHeaders;
 import ru.itis.test.services.VideoService;
 
-import java.io.File;
+import java.util.Objects;
 
 
 @RestController
@@ -19,17 +19,15 @@ public class UploadController {
     @Autowired
     private VideoService videoService;
 
+    private final String MP4_CONTENT_TYPE = "video/mp4";
+
     @PostMapping("/uploadVideo")
     public ResponseEntity<?> uploadVideo(
             @RequestParam("video") final MultipartFile multipartFile) {
-        return errorHandle(videoService.uploadVideo(multipartFile));
-    }
-
-    private ResponseEntity errorHandle(final File file) {
-        if (file == null) {
-            return new ResponseEntity<>("Ошибка загрузки", new HttpHeaders(), HttpStatus.OK);
+        if (!Objects.equals(multipartFile.getContentType(), MP4_CONTENT_TYPE)) {
+            return new ResponseEntity<>("Invalid content-type", new HttpHeaders(), HttpStatus.OK);
         }
-        return new ResponseEntity<>("Success", new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(videoService.uploadVideo(multipartFile), new HttpHeaders(), HttpStatus.OK);
     }
 
 }
--- src/main/java/ru/itis/test/services/implementations/VideoServiceImplementation.java
+++ src/main/java/ru/itis/test/services/implementations/VideoServiceImplementation.java
@@ -1,21 +1,21 @@
 package ru.itis.test.services.implementations;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.mp4parser.IsoFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.itis.test.configuration.DefaultFileSavingPathConfiguration;
 import ru.itis.test.services.VideoService;
-import com.xuggle.xuggler.IContainer;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.UUID;
-
-import org.apache.commons.io.FilenameUtils;
 
 import static ru.itis.test.configuration.DefaultFileSavingPathConfiguration.SAVING_FOLDER;
 
@@ -24,38 +24,47 @@ public class VideoServiceImplementation implements VideoService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VideoServiceImplementation.class);
 
+    private IsoFile isoFile;
+
+    @Value("#{new Integer('${uploading.maximumVideoDuration}')}")
+    private Integer maximumVideoDuration;
+
     @Override
-    public File uploadVideo(final MultipartFile multipartFile) {
+    public String uploadVideo(final MultipartFile multipartFile) {
         final UUID fileName = UUID.randomUUID();
         final String fileExtension = Objects.requireNonNull(FilenameUtils.getExtension(multipartFile.getOriginalFilename()));
         final String fullFileName = fileName.toString() + "." + fileExtension;
-
-        final File newFile = new File(SAVING_FOLDER, fullFileName);
+        final String fullFilePath = SAVING_FOLDER + File.separator + fullFileName;
+        final File newFile = new File(fullFilePath);
         if (!multipartFile.getOriginalFilename().isEmpty()) {
             LOGGER.info("Saving video {} to LOCAL_STORAGE", fullFileName);
-            //save file to local storage
             final File defaultSavingFolder = DefaultFileSavingPathConfiguration.initSavingFolder();
             try (final InputStream inputStream = multipartFile.getInputStream()) {
                 FileUtils.copyInputStreamToFile(inputStream, newFile);
-
-                IContainer container = IContainer.make();
+                isoFile = new IsoFile(fullFilePath);
+                double lengthInSeconds = (double)
+                        isoFile.getMovieBox().getMovieHeaderBox().getDuration() /
+                        isoFile.getMovieBox().getMovieHeaderBox().getTimescale();
+                if (lengthInSeconds > maximumVideoDuration) {
+                    deleteFile(newFile);
+                    return "Invalid duration";
+                }
                 for (final File existingFile : Objects.requireNonNull(defaultSavingFolder.listFiles())) {
                     if (FileUtils.contentEquals(existingFile, newFile)) {
                         deleteFile(newFile);
-                        return null;
+                        return "File already exists";
                     }
                 }
 
             } catch (final IOException e) {
                 LOGGER.error("Error saving file {}", fullFileName);
-                return null;
+                return "Error saving file";
             }
 
-            return newFile;
+            return "OK";
         } else {
-            return null;
+            return "Server error - empty file";
         }
-
 
     }
 
